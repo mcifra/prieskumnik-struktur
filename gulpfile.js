fe9dcd7de321c349c60bd8dc3cdfdf8cc8a17bbd
milan.cifra <cifra9@uniba.sk>
2017-10-25 22:47:44 +0200

    gulp - change css destination, styles

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require("gulp-sass");
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass:watch', function () {
