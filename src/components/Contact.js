e080851cd449cbe7a90d7d017ae4f84a8ab8ad9b
milan.cifra <cifra9@uniba.sk>
2017-11-01 20:30:16 +0100

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 class Contact extends React.Component {
     render() {
         return (
-            <h1>Kontakt</h1>
+            <div className={"contact"}>
+                <h1>Kontakt</h1>
+                <Grid>
+                    <Row className="show-grid block">
+                        <Col md={6} className="center">
+                            <h2>Meno</h2>
+                            <p>Milan Cifra</p>
+                        </Col>
+                        <Col md={6} className="center">
+                            <h2>Email</h2>
+                            <p>cifra9@uniba.sk</p>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
         );
     }
 }
