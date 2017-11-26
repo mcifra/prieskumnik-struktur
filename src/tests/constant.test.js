63b536dedfbe7f6a7df678db32ff81ea46fa88b3
milan.cifra <cifra9@uniba.sk>
2017-11-26 19:09:23 +0100

    constant tests

---
+++ src/tests/constant.test.js
@@ -0,0 +1,30 @@
+import Language from "../backend/Language";
+import Structure from "../backend/Structure";
+import Constant from "../backend/term/Term.Constant";
+
+var l = new Language();
+l.addConstant("C1");
+
+var s = new Structure(l);
+s.addDomainItem("c");
+s.setConstantValue("C1","c");
+s.setConstantValue("C2", "d");
+
+var e = new Map();
+
+var c1 = new Constant("C1");
+var c2 = new Constant("C2");
+var c3 = new Constant("C3");
+
+
+test("t1", () => {
+    expect(c1.toString()).toBe("C1");
+});
+
+test("t2", () => {
+    expect(c1.interpret(s, e)).toBe("c");
+});
+
+test("t3", () => {
+    expect(c2.interpret(s, e)).toBe("d");
+});
