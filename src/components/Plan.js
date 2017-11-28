9ec0cdbf97624e61659d3eac12a2b1eb5b6a3a1f
milan.cifra <cifra9@uniba.sk>
2017-11-28 19:17:38 +0100

    plan, references

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -8,10 +8,11 @@ class Plan extends React.Component {
                 <p>
                     Tu môžete vidieť ako som si rozvrhol časový plán tvorby práce. Plán je len orientačný, reálne dátumy dokončenia úloh sa môžu líšiť.
                 </p>
-                <p>Posledná aktualizácia: 10.11.2017</p>
+                <p>Posledná aktualizácia: 26.11.2017</p>
                 <table className={"table table-hover"}>
                     <thead>
                     <tr>
+                        <th>Začiatok</th>
                         <th>Deadline</th>
                         <th>Úloha</th>
                     </tr>
@@ -20,7 +21,10 @@ class Plan extends React.Component {
                     <tbody>
                     <tr>
                         <td>
-                            <b>24.11.2017</b>
+                            <b>10.11.2017</b>
+                        </td>
+                        <td>
+                            <b>26.11.2017</b>
                         </td>
                         <td>
                             Model aplikácie - vytvorenie "backendu" aplikácie. Tvorba hierarchie tried ako sú jednotlivé podformuly (disjunkcia, konjunkcia,
@@ -30,7 +34,10 @@ class Plan extends React.Component {
                     </tr>
                     <tr>
                         <td>
-                            <b>08.12.2017</b>
+                            <b>27.11.2017</b>
+                        </td>
+                        <td>
+                            <b>10.12.2017</b>
                         </td>
                         <td>
                             Parser formúl - bude potrebný na prevod textového reťazca do jednotlivých tried a opačne.
@@ -38,7 +45,10 @@ class Plan extends React.Component {
                     </tr>
                     <tr>
                         <td>
-                            <b>22.12.2017</b>
+                            <b>11.12.2017</b>
+                        </td>
+                        <td>
+                            <b>24.12.2017</b>
                         </td>
                         <td>
                             Implementácia UI #1 - učenie sa tvorby UI v React-e, vytvorenie UI pre interaktívne editovanie štruktúry a jazyka
@@ -46,7 +56,10 @@ class Plan extends React.Component {
                     </tr>
                     <tr>
                         <td>
-                            <b>05.01.2018</b>
+                            <b>01.01.2018</b>
+                        </td>
+                        <td>
+                            <b>14.01.2018</b>
                         </td>
                         <td>
                             Implementácia UI #2 - interaktívne editovanie predikátových symbolov (zložitejšia časť)
@@ -54,20 +67,60 @@ class Plan extends React.Component {
                     </tr>
                     <tr>
                         <td>
-                            <b>19.01.2018</b>
+                            <b>15.01.2018</b>
                         </td>
                         <td>
-                            Implementácia UI #3 - učiteľská časť a dokončenie
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <b>15.02.2018</b>
+                            <b>28.01.2018</b>
                         </td>
                         <td>
                             Písanie východiskovej kapitoly a dokončenie prototypu
                         </td>
                     </tr>
+                    <tr>
+                        <td>
+                            <b>22.01.2018</b>
+                        </td>
+                        <td>
+                            <b>11.02.2018</b>
+                        </td>
+                        <td>
+                            Implementácia UI #3 - učiteľská časť, JSON import & export
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>12.02.2018</b>
+                        </td>
+                        <td>
+                            <b>15.04.2018</b>
+                        </td>
+                        <td>
+                            Vyľaďovanie a vylepšovanie UI
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>15.04.2018</b>
+                        </td>
+                        <td>
+                            <b> </b>
+                        </td>
+                        <td>
+                            Testovanie aplikácie na predmente Matematika (4) - Logika pre informatikov
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>02.04.2018</b>
+                        </td>
+                        <td>
+                            <b>06.05.2018</b>
+                        </td>
+                        <td>
+                            Písanie práce
+                        </td>
+                    </tr>
+
                     </tbody>
                 </table>
 
