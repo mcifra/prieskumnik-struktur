ad27f4f73e033dceca6fbe928273be84f5488062
mcifra <milan3362@gmail.com>
2018-02-04 23:29:56 +0100

    deleted help messages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -22,43 +22,8 @@ class App extends React.Component {
 
     render() {
         console.log('STRUCTURE', this.state.structure);
-        let c = [...this.state.structure.language.constants];
-        let p = [...this.state.structure.language.predicates];
-        let f = [...this.state.structure.language.functions];
-        let d=[...this.state.structure.domain];
-        let cStr = '';
-        let pStr='';
-        let fStr='';
-        let dStr='';
-        for (let i = 0; i < c.length; i++) {
-            cStr += c[i] + ' ';
-        }
-        for (let i = 0; i < p.length; i++) {
-            pStr += p[i][0] + ' ';
-        }
-        for (let i = 0; i < f.length; i++) {
-            fStr += f[i][0] + ' ';
-        }
-        for (let i = 0; i < d.length; i++) {
-            dStr += d[i] + ' ';
-        }
-        console.log(cStr);
         return (
             <div className={"app"}>
-                <ul>
-                    <li>
-                        {cStr}
-                    </li>
-                    <li>
-                        {pStr}
-                    </li>
-                    <li>
-                        {fStr}
-                    </li>
-                    <li>
-                        {dStr}
-                    </li>
-                </ul>
                 <div className={"col-lg-6"}>
                     <LanguageEditor structure={this.state.structure}
                                     onChange={(structure) => this.setStructure(structure)}/>
