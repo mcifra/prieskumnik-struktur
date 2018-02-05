92167be14b72e9115a4ec81c35e4e38e92c54450
mcifra <milan3362@gmail.com>
2018-02-05 02:15:36 +0100

    formula storage design update

--- src/components/app/FormulaStorage.js
+++ src/components/app/FormulaStorage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Row, Col, Panel, Button, FormGroup, InputGroup, FormControl} from 'react-bootstrap';
+
 import Language from "../../backend/Language";
 import Conjunction from "../../backend/formula/Formula.Conjunction";
 import Disjunction from "../../backend/formula/Formula.Disjunction";
@@ -23,35 +25,53 @@ class FormulaStorage extends React.Component {
 
     render() {
         return (
-            <div className={"formula-storage"}>
-                <h2>Formuly</h2>
-                {
-                    this.state.formulas.map((current, index) =>
-                        <div className={'row'} key={index}>
-                            <div className={'col-lg-6'}>
-                                {
-                                    current.validationMessage != '' ? (
-                                        <div className={'alert alert-danger'}>
-                                            {current.validationMessage}
-                                        </div>
-                                    ) : ''
-                                }
-                                <div className={'input-group'} key={index}>
-                                    <span className={"input-group-addon"}
-                                          htmlFor={"predicates-list"}>{current.ok}</span>
-                                    <input className={'form-control'} type={"text"} value={current.formula}
-                                           onChange={(e) => this.checkFormula(e, index)}/>
-                                    <span className={'input-group-btn'}>
-                                        <button className={'btn btn-danger'}
-                                                onClick={() => this.deleteFormula(index)}>Odstrániť</button>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-                <button className={'btn btn-primary'} id={"add-formula"} onClick={() => this.addFormula()}>Pridaj
-                </button>
+            <div className='formula-storage'>
+                <Panel>
+                    <Panel.Heading>
+                        <Panel.Title>Formuly</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body>
+                        {
+                            this.state.formulas.map((current, index) =>
+                                <Row key={index}>
+                                    <Col lg={12}>
+                                        <FormGroup>
+                                            <InputGroup>
+                                                <FormControl type='text' value={current.formula}
+                                                             onChange={(e) => this.checkFormula(e, index)}/>
+                                                <InputGroup.Button>
+                                                    <Button onClick={() => this.deleteFormula(index)}>✖</Button>
+                                                </InputGroup.Button>
+                                            </InputGroup>
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                // <div className={'row'} key={index}>
+                                //     <div className={'col-lg-6'}>
+                                //         {
+                                //             current.validationMessage != '' ? (
+                                //                 <div className={'alert alert-danger'}>
+                                //                     {current.validationMessage}
+                                //                 </div>
+                                //             ) : ''
+                                //         }
+                                //         <div className={'input-group'} key={index}>
+                                //     <span className={"input-group-addon"}
+                                //           htmlFor={"predicates-list"}>{current.ok}</span>
+                                //             <input className={'form-control'} type={"text"} value={current.formula}
+                                //                    onChange={(e) => this.checkFormula(e, index)}/>
+                                //             <span className={'input-group-btn'}>
+                                //         <button className={'btn btn-danger'}
+                                //                 onClick={() => this.deleteFormula(index)}>Odstrániť</button>
+                                //     </span>
+                                //         </div>
+                                //     </div>
+                                // </div>
+                            )
+                        }
+                        <Button bsStyle='success' onClick={() => this.addFormula()}>➕ Pridaj formulu</Button>
+                    </Panel.Body>
+                </Panel>
             </div>
         );
     }
